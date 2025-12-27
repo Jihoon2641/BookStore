8db1d8eb92b7cdefc2d205eb1d5cd3e500fb971b
조지훈 <jihoon@jojihun-ui-MacBookAir.local>
2025-12-27 16:50:33 +0900

    feat: 사용자 도메인 추가
    비즈니스 로직을 담는 사용자 도메인 작성

---
+++ bookstore-api/src/main/java/com/bookstore/bookstore_api/domain/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.bookstore.bookstore_api.domain.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Column;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", nullable = false)
+    private String updatedBy;
+}
---
+++ bookstore-api/src/main/java/com/bookstore/bookstore_api/domain/entity/user/UserEntity.java
@@ -0,0 +1,28 @@
+package com.bookstore.bookstore_api.domain.entity.user;
+
+import com.bookstore.bookstore_api.domain.entity.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class UserEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
