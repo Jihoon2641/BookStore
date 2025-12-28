c99e011166ad3d4556e44a22495875ec7d5e62d6
조지훈 <jihoon@jojihun-ui-MacBookAir.local>
2025-12-28 20:51:17 +0900

    chore : 주석 수정

--- bookstore-api/src/main/java/com/bookstore/bookstore_api/util/validate/SelfValidating.java
+++ bookstore-api/src/main/java/com/bookstore/bookstore_api/util/validate/SelfValidating.java
@@ -10,7 +10,7 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
 /**
- * 도메인 객체의 유효성 검증을 위한 클래스
+ * 커맨드 객체(DTO)의 유효성 검증을 위한 클래스
  * 객체가 생성될 때 자동으로 유효성을 검증하고, 유효하지 않은 경우 예외를 발생시킨다.
  */
 public abstract class SelfValidating<T> {
