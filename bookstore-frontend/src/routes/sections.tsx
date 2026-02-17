042cfc28c6a9dad31cf7801334c6cc3537b72923
조지훈 <dev.jihoon2641@gmail.com>
2026-02-17 16:06:00 +0900

    refactor : aside 정리 및 불필요한 템플릿 제거

--- bookstore-frontend/src/routes/sections.tsx
+++ bookstore-frontend/src/routes/sections.tsx
@@ -13,7 +13,6 @@ import { DashboardLayout } from 'src/layouts/dashboard';
 // ----------------------------------------------------------------------
 
 export const DashboardPage = lazy(() => import('src/pages/dashboard'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const SignInPage = lazy(() => import('src/pages/sign-in'));
 export const SignUpPage = lazy(() => import('src/pages/sign-up'));
@@ -53,7 +52,6 @@ export const routesSection: RouteObject[] = [
       { index: true, element: <DashboardPage /> },
       { path: 'user', element: <UserPage /> },
       { path: 'products', element: <ProductsPage /> },
-      { path: 'blog', element: <BlogPage /> },
     ],
   },
   {
